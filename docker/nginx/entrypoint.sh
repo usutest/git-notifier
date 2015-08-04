53cfa826f5f26a5a8deabe9525ffbed2a009ef1b
Andrea Usuelli <andrea.usuelli@rocket-internet.de>
2015-08-04 17:03:23 +0200

    wip

---
+++ docker/nginx/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+sed -i 's/APP_DOMAIN_NAME/'$APP_DOMAIN_NAME'/g' /etc/nginx/conf.d/gitnotifier
+
+sed -i 's/APP_UPSTREAM_PUMA/'$APP_UPSTREAM_PUMA'/g' /etc/nginx/conf.d/gitnotifier
+
+sed -i 's/APP_RESOLVER_NGINX/'$APP_RESOLVER_NGINX'/g' /etc/nginx/conf.d/gitnotifier
+
+nginx -c /etc/nginx/nginx.conf
